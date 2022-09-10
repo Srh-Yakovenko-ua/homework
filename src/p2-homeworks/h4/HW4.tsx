407b856ebb70839158f7c8e56278ef6b61fa6d7f
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-09-10 19:44:15 +0300

    fix

--- src/p2-homeworks/h4/HW4.tsx
+++ src/p2-homeworks/h4/HW4.tsx
@@ -58,8 +58,7 @@ function HW4() {
 
                 <SuperCheckbox
                     checked={checked}
-                    onChangeChecked={setChecked}
-                >
+                    onChangeChecked={setChecked}>
                     some text {/*// этот текст попадёт в children*/}
                 </SuperCheckbox>
 
