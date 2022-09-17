9f1ad07e83f2135361bd2d27c3edeee9cfbc22fd
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-09-17 21:46:42 +0300

    fix

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import classes from './Header.module.css'
+import {NavLink} from 'react-router-dom';
+import {PATH} from './Pages';
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
 
+    const activeJunior = ({isActive}: any) => isActive ? classes.active : classes.nonActive
+    const activePreJunior = ({isActive}: any) => isActive ? classes.active : classes.nonActive
+    const activeJuniorPlus = ({isActive}: any) => isActive ? classes.active : classes.nonActive
+
+    return (
+        <div className={classes.dropdown}>
+            <button className={classes.dropbtn}></button>
+            <div className={classes.dropdownContent}>
+                <NavLink to={PATH.PRE_JUNIOR} className={activeJunior}>pre_junior</NavLink>
+                <NavLink to={PATH.JUNIOR} className={activePreJunior}>junior</NavLink>
+                <NavLink to={PATH.JUNIOR_PLUS} className={activeJuniorPlus}>junior_plus</NavLink>
+            </div>
         </div>
     )
 }
+
 
 export default Header
