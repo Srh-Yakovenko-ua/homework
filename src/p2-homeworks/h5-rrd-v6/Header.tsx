ec1c6cfad1fd9dc1ef860b53ffd8bf2e20f4b8d8
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-09-18 14:00:36 +0300

    change classes

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -5,17 +5,15 @@ import {PATH} from './Pages';
 
 function Header() {
 
-    const activeJunior = ({isActive}: { isActive: boolean }) => isActive ? classes.active : ''
-    const activePreJunior = ({isActive}: { isActive: boolean }) => isActive ? classes.active : ''
-    const activeJuniorPlus = ({isActive}: { isActive: boolean }) => isActive ? classes.active : ''
+    const activeClassName = ({isActive}: { isActive: boolean }) => isActive ? classes.active : ''
 
     return (
         <div className={classes.dropdown}>
             <button className={classes.dropbtn}></button>
             <div className={classes.dropdownContent}>
-                <NavLink to={PATH.PRE_JUNIOR} className={activeJunior}>pre_junior</NavLink>
-                <NavLink to={PATH.JUNIOR} className={activePreJunior}>junior</NavLink>
-                <NavLink to={PATH.JUNIOR_PLUS} className={activeJuniorPlus}>junior_plus</NavLink>
+                <NavLink to={PATH.PRE_JUNIOR} className={activeClassName}>pre_junior</NavLink>
+                <NavLink to={PATH.JUNIOR} className={activeClassName}>junior</NavLink>
+                <NavLink to={PATH.JUNIOR_PLUS} className={activeClassName}>junior_plus</NavLink>
             </div>
         </div>
     )
