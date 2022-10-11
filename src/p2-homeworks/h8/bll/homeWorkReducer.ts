763f789ce10a6b48d31c266cad92053d1a7c018d
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-10-11 22:08:29 +0300

    fix reducer

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,36 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from '../HW8';
+
+type ActionsType = {
+    type: 'sort',
+    payload: 'up' | 'down'
+} | { type: 'check', payload: number }
+
+export const homeWorkReducer = (state: UserType[], action: ActionsType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            const newState = [...state];
+            newState.sort(function (a, b) {
+                if (action.payload === 'up') {
+                    return a.name < b.name ? 1 : -1
+                } else if (action.payload === 'down') {
+                    return a.name > b.name ? 1 : -1
+                } else return 0
+            })
+            return newState;
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(el => el.age > action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
+
+// const newState = [...state]
+// if (action.payload === 'up') {
+//    newState.sort((a, b) => a.name < b.name ? -1 : 1)
+// }
+// if (action.payload === 'down') {
+//  newState.sort((a, b) => a.name > b.name ? 1 : -1)
+// }
+// return  newState
