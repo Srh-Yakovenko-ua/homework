6188baacd4182fa37bf79ec0b8ecc9a0c779691d
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-11-03 13:27:03 +0300

    fixed

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,20 +1,35 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
-import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange';
+
+const minDistance = 10
 
 function HW11() {
     const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+    const [value2, setValue2] = useState(100);
+
+
+    const changeTwoValueSliders = (value: [number, number] | number[]) => {
+        setValue1(value[0])
+        setValue2(value[1])
+    }
+
+    const changeValueFirstSlider = (value: number) => {
+        if (value + minDistance < value2) setValue1(value)
+    }
+
 
     return (
         <div>
             <hr/>
+
             homeworks 11
 
             {/*should work (должно работать)*/}
             <div>
                 <span>{value1}</span>
-                <SuperRange
+                <SuperRange onChangeRange={changeValueFirstSlider}
+                            value={value1}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
@@ -22,8 +37,10 @@ function HW11() {
             <div>
                 <span>{value1}</span>
                 <SuperDoubleRange
+                    onChangeRange={changeTwoValueSliders}
                     // сделать так чтоб value1 и value2 изменялось
-                />
+                    minDistance={minDistance}
+                    value={[value1, value2]}/>
                 <span>{value2}</span>
             </div>
 
