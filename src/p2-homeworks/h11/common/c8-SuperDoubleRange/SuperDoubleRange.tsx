be4bf8290c32f674adc13321cd390ec935a9d733
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-11-03 13:26:46 +0300

    fixed

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,24 +1,51 @@
 import React from 'react'
+import {Box, Slider} from '@mui/material';
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
+    onChangeRange?: (value: [number, number] | number[]) => void
+    value: [number, number]
+    minDistance: number
     // min, max, step, disable, ...
+}
+
+function valuetext(value: number) {
+    return `${value}°C`;
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
-        onChangeRange, value,
-        // min, max, step, disable, ...
+        onChangeRange, value, minDistance, ...restProps
+        // min, max, step, disable,
     }
 ) => {
     // сделать самому, можно подключать библиотеки
 
+    const handleChange = (
+        event: Event,
+        newValue: number | number[],
+        activeThumb: number,
+    ) => {
+        if (!Array.isArray(newValue)) return
+
+        return activeThumb === 0 ?
+            onChangeRange && onChangeRange([Math.min(newValue[0], newValue[1] - minDistance), newValue[1]]) :
+            onChangeRange && onChangeRange([newValue[0], Math.max(newValue[1], newValue[0] + minDistance)])
+    }
+
+
+
     return (
-        <>
-            DoubleRange
-        </>
+        <Box sx={{width: 150, display: 'inline-block', margin: '0 10px'}}>
+            <Slider
+                getAriaLabel={() => 'Minimum distance'}
+                value={value}
+                onChange={handleChange}
+                valueLabelDisplay="auto"
+                getAriaValueText={valuetext}
+                disableSwap
+                {...restProps}
+            />
+        </Box>
     )
 }
-
 export default SuperDoubleRange
